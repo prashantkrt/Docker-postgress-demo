83550e9d0cdd4b721d94902dd6cf11b84fbd8f51
prashanttiwary10@gmail.com <prashanttiwary10@gmail.com>
2024-09-01 21:59:18 +0530

    docker-compose

--- src/main/java/com/mylearning/docker/deployment/dockerpostgressdemo/DockerPostgressDemoApplication.java
+++ src/main/java/com/mylearning/docker/deployment/dockerpostgressdemo/DockerPostgressDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerPostgressDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(DockerPostgressDemoApplication.class, args);
     }
 
